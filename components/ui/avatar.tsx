dd77c57de7152e124f49855adf5dc40f0ad10afe
Ankit Pavar <ankitpavar7@gmail.com>
2025-02-13 17:01:34 +0530

    chore; removed border from avatar

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center border-1 border-primary rounded-full bg-muted",
+      "flex h-full w-full items-center justify-center  rounded-full bg-muted",
       className
     )}
     {...props}
